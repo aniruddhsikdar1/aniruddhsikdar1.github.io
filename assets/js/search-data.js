c71b8a7aa04d2606d09ca6165b558e1c77201f39
aniruddhsikdar1 <anirudhsikdar3@gmail.com>
2025-10-08 17:09:46 +0000

    Deploying to gh-pages from @ aniruddhsikdar1/aniruddhsikdar1.github.io@d207dd87ed322f2698b5fab33f43bc0a8131a3b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-service",
+          title: "Service",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
