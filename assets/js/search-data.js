2aca3b6ab6536021b4d01723b52723f5c21c14a8
aniruddhsikdar1 <anirudhsikdar3@gmail.com>
2025-10-12 16:30:40 +0000

    Deploying to gh-pages from @ aniruddhsikdar1/aniruddhsikdar1.github.io@3ed490d14a572de8a99346eaf734b4d44f1e0f88 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-competitions",
+          title: "Competitions",
           description: "",
           section: "Navigation",
           handler: () => {
